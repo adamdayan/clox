05197d2c02680d9a89418db15b5c30969321cf55
Adam Dayan <nihao10@gmail.com>
2025-05-05 19:58:53 +0100

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "clox bytecode interpreter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"
+      ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            clang
+            clang-tools
+          ];
+        };
+      };
+    };
+}
